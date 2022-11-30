87c73d25d06aacae1a28f69776c2dcdd12ab4e91
classic-k <classicconceptone@gmail.com>
2022-11-30 15:51:05 +0000

    deploy

--- src/config.ts
+++ src/config.ts
@@ -7,5 +7,5 @@ export const authConfig = {
   // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'dev-8b0fovvy.us.auth0.com',            // Auth0 domain
   clientId: '4Owg6QtJsHke87medLYUUzKdq99tayB3',          // Auth0 client id
-  callbackUrl: "https://8080-cs-837858227023-default.cs-europe-west1-onse.cloudshell.dev/callback"
+  callbackUrl: "https://classic-k.github.io/callback"
 }
