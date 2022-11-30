f939ecae8bf391d5b39713597bdfb8be307d6d21
classic-k <classicconceptone@gmail.com>
2022-11-30 22:00:45 +0000

    callback

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'ce99460ca4'
+const apiId = 'cd6qpav3ad'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
@@ -7,5 +7,5 @@ export const authConfig = {
   // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'dev-8b0fovvy.us.auth0.com',            // Auth0 domain
   clientId: '4Owg6QtJsHke87medLYUUzKdq99tayB3',          // Auth0 client id
-  callbackUrl: "https://classic-k.github.io/Udagram-Todo-Client/callback"
+  callbackUrl: "https://8080-cs-837858227023-default.cs-europe-west1-onse.cloudshell.dev/callback"
 }
